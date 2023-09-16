51fa46e00e47e8b005cc4f27d31d22decbdcfe2e
MorozovMI <misha.morozov0202@bk.ru>
2023-09-16 13:34:22 +0500

    Initial commit

---
+++ src/test/java/ru/morozov/testmvcfirstapp/TestMvcFirstAppApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.morozov.testmvcfirstapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestMvcFirstAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
